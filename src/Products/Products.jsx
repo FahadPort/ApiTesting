e0d6cdef7a38d9289cf7025715f076d6d909e4d3
Muhammad Fahad <muhammadfahad3017774@gmail.com>
2024-04-27 13:32:26 -0700

    done

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,68 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Container } from 'react-bootstrap';
-import { Link } from "react-router-dom";
-import Productimg from "../Assets/Images/Product01.jpg"
-
-
-
-
-
-
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Sdata from '../SData/Sdata';
 
 const Products = () => {
-
-   
-
-
-
+    console.log(Sdata[0])
   return (
-   <>
-
-   <div id="products">
-    <Container>
-    <div className="product-inner">
-    <Row>
-        <Col md={3}>
-            <div className="product-wrapper">
-                <div className="image">
-                    <img src={Productimg} alt="" />
-                </div>
-                <div className="product-info">
-                    <div className="product-module">
-                        <div className="product-sku"><p>SKU#</p></div>
-                        <div className="product-model">EQ702438U</div>
+    <>
+      <div id="products">
+        <Container>
+          <div className="product-inner">
+            <Row>
+              {Sdata.map((val, index) => (
+                <Col key={index} md={3}>
+                  <div className="product-wrapper">
+                    <div className="image">
+                    <img src={val.proimg} alt="" />
                     </div>
-                    <div className="product-title">
-                        <h3>Harris NEXIO NX AMP 3801 HDI</h3>
+                    <div className="product-info">
+                      <div className="product-module">
+                        <div className="product-sku">
+                          <p>SKU#</p>
+                        </div>
+                        <div className="product-model">{val.sku}</div>
+                      </div>
+                      <div className="product-title">
+                        <h3>{val.protitle}</h3>
+                      </div>
+                      <div className="product-des">
+                        <p>{val.prodes}</p>
+                      </div>
+                      <div className="product-btn">
+                        <button className="cta-btn">
+                          <Link to="/">Add to cart</Link>
+                        </button>
+                      </div>
                     </div>
-                    <div className="product-des">
-                        <p>Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID</p>
-                    </div>
-                    <div className="product-btn">
-                        <button className='cta-btn'><Link to={"/"}>Add to cart</Link></button>
-                    </div>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-                </div>
-            </div>
-
-
-        </Col>
-
-    </Row>
-   
-   </div>
-   </Container>
-   </div>
-   
-   
-   </>
-
-
-
-  )
-}
-
-export default Products
+export default Products;
--- src/SData/Sdata.jsx
+++ src/SData/Sdata.jsx
@@ -2,34 +2,34 @@ const Sdata = [
     {
         proimg: "../Assets/Images/Product01.jpg",
 sku: "EQ702438U",
-protitle: "Harris NEXIO NX AMP 3801 HDI",
+protitle: "01 Harris NEXIO NX AMP 3801 HDI",
+prodes: "The Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
+
+    },
+    {
+        proimg: "../Assets/Images/Product01.jpg",
+sku: "EQ702438U",
+protitle: "02 Harris NEXIO NX AMP 3801 HDI",
 prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
 
     },
     {
         proimg: "../Assets/Images/Product01.jpg",
 sku: "EQ702438U",
+protitle: "03 Harris NEXIO NX AMP 3801 HDI",
+prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
+
+    },
+    {
+        proimg: "../Assets/Images/Product01.jpg",
+sku: " 04 EQ702438U",
 protitle: "Harris NEXIO NX AMP 3801 HDI",
 prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
 
     },
     {
         proimg: "../Assets/Images/Product01.jpg",
-sku: "EQ702438U",
-protitle: "Harris NEXIO NX AMP 3801 HDI",
-prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
-
-    },
-    {
-        proimg: "../Assets/Images/Product01.jpg",
-sku: "EQ702438U",
-protitle: "Harris NEXIO NX AMP 3801 HDI",
-prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
-
-    },
-    {
-        proimg: "../Assets/Images/Product01.jpg",
-sku: "EQ702438U",
+sku: "05 EQ702438U",
 protitle: "Harris NEXIO NX AMP 3801 HDI",
 prodes: "Broadcast video server integrated with 8SD or 4 HD channels, with channelView option and 4TB RAID",
 
